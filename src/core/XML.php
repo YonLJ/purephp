24de877c889ddc9dd104310bef28575e647d7f24
YonLD <istintin@qq.com>
2023-12-29 22:30:56 +0800

    * split the core into Dom, HTML, XML, SVG and Raw classes.

---
+++ src/core/XML.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+namespace Tiny\Core;
+
+class XML extends Tag {
+    public static function __callStatic(string $tag, array $children): XML
+    {
+        return new XML($tag, $children);
+    }
+
+    public function save(string $path, string $header = '<?xml version="1.0"?>'): int|false
+    {
+        $handle = fopen($path, 'w');
+        if ($handle === false) {
+            return false;
+        }
+
+        $result = fwrite($handle, $header . (string)$this->toTDom());
+        fclose($handle);
+        return $result;
+    }
+}
