db03056307a5049f67107213ce2bfbd5c8711ca3
YonLJ <istintin@qq.com>
2023-12-29 22:33:25 +0800

    + set up PHPUnit and add HTML, XML and utils tests.

---
+++ tests/UtilsTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use function Tiny\Utils\clx;
+use function Tiny\Utils\sty;
+
+class UtilsTest extends TestCase
+{
+    public function testClx()
+    {
+        $this->assertEquals('class-a class-b class-c class-d', clx(
+            'class-a',
+            'class-b',
+            [
+                'class-c',
+                'class-d' => true,
+                'class-e' => false
+            ]
+        ));
+        $this->assertEquals('class-a class-b class-c', clx(
+            null,
+            '',
+            [
+                'class-a',
+                'class-b'
+            ],
+            [
+                'class-c' => true,
+                null
+            ]
+        ));
+    }
+
+    public function testSty()
+    {
+        $this->assertEquals('background-color: red; height: 36px; border: 1px solid #fff;', sty([
+            'background-color' => 'red',
+            'height' => '36px',
+            'border' => '1px solid #fff'
+        ]));
+    }
+}
