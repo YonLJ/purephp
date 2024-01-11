9ad4daf61549cd0ee86955e0c753700214fd5577
YonLJ <istintin@qq.com>
2024-01-11 17:51:16 +0800

    * move the rawHtml() and rawXml() helpers from core to Pure\Utils.

---
+++ src/utils/raw.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+namespace Pure\Utils;
+
+use Pure\Core\Raw;
+use Pure\Core\RawType;
+
+function rawHtml(string $content): Raw
+{
+    return new Raw(RawType::HTML, $content);
+}
+
+function rawXml(string $content): Raw
+{
+    return new Raw(RawType::XML, $content);
+}
