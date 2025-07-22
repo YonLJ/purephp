94a60d5534dacda32ad184ac82b0ef1a2c76be86
YonLJ <istintin@qq.com>
2025-07-22 23:20:03 +0800

    * make Tag, HTML and XML constructors public and extend the SVG/XML tests.

--- src/tags/html.php
+++ src/tags/html.php
@@ -8,441 +8,441 @@ use Pure\Core\HTML;
 
 function a(): HTML
 {
-    return HTML::a(...func_get_args());
+    return new HTML('a', func_get_args());
 }
 function abbr(): HTML
 {
-    return HTML::abbr(...func_get_args());
+    return new HTML('abbr', func_get_args());
 }
 function address(): HTML
 {
-    return HTML::address(...func_get_args());
+    return new HTML('address', func_get_args());
 }
 function area(): HTML
 {
-    return HTML::area(...func_get_args());
+    return new HTML('area', func_get_args());
 }
 function article(): HTML
 {
-    return HTML::article(...func_get_args());
+    return new HTML('article', func_get_args());
 }
 function aside(): HTML
 {
-    return HTML::aside(...func_get_args());
+    return new HTML('aside', func_get_args());
 }
 function audio(): HTML
 {
-    return HTML::audio(...func_get_args());
+    return new HTML('audio', func_get_args());
 }
 function b(): HTML
 {
-    return HTML::b(...func_get_args());
+    return new HTML('b', func_get_args());
 }
 function base(): HTML
 {
-    return HTML::base(...func_get_args());
+    return new HTML('base', func_get_args());
 }
 function bdi(): HTML
 {
-    return HTML::bdi(...func_get_args());
+    return new HTML('bdi', func_get_args());
 }
 function bdo(): HTML
 {
-    return HTML::bdo(...func_get_args());
+    return new HTML('bdo', func_get_args());
 }
 function blockquote(): HTML
 {
-    return HTML::blockquote(...func_get_args());
+    return new HTML('blockquote', func_get_args());
 }
 function body(): HTML
 {
-    return HTML::body(...func_get_args());
+    return new HTML('body', func_get_args());
 }
 function br(): HTML
 {
-    return HTML::br(...func_get_args());
+    return new HTML('br', func_get_args());
 }
 function button(): HTML
 {
-    return HTML::button(...func_get_args());
+    return new HTML('button', func_get_args());
 }
 function canvas(): HTML
 {
-    return HTML::canvas(...func_get_args());
+    return new HTML('canvas', func_get_args());
 }
 function caption(): HTML
 {
-    return HTML::caption(...func_get_args());
+    return new HTML('caption', func_get_args());
 }
 function cite(): HTML
 {
-    return HTML::cite(...func_get_args());
+    return new HTML('cite', func_get_args());
 }
 function code(): HTML
 {
-    return HTML::code(...func_get_args());
+    return new HTML('code', func_get_args());
 }
 function col(): HTML
 {
-    return HTML::col(...func_get_args());
+    return new HTML('col', func_get_args());
 }
 function colgroup(): HTML
 {
-    return HTML::colgroup(...func_get_args());
+    return new HTML('colgroup', func_get_args());
 }
 function data(): HTML
 {
-    return HTML::data(...func_get_args());
+    return new HTML('data', func_get_args());
 }
 function datalist(): HTML
 {
-    return HTML::datalist(...func_get_args());
+    return new HTML('datalist', func_get_args());
 }
 function dd(): HTML
 {
-    return HTML::dd(...func_get_args());
+    return new HTML('dd', func_get_args());
 }
 function del(): HTML
 {
-    return HTML::del(...func_get_args());
+    return new HTML('del', func_get_args());
 }
 function details(): HTML
 {
-    return HTML::details(...func_get_args());
+    return new HTML('details', func_get_args());
 }
 function dfn(): HTML
 {
-    return HTML::dfn(...func_get_args());
+    return new HTML('dfn', func_get_args());
 }
 function dialog(): HTML
 {
-    return HTML::dialog(...func_get_args());
+    return new HTML('dialog', func_get_args());
 }
 function div(): HTML
 {
-    return HTML::div(...func_get_args());
+    return new HTML('div', func_get_args());
 }
 function dl(): HTML
 {
-    return HTML::dl(...func_get_args());
+    return new HTML('dl', func_get_args());
 }
 function dt(): HTML
 {
-    return HTML::dt(...func_get_args());
+    return new HTML('dt', func_get_args());
 }
 function em(): HTML
 {
-    return HTML::em(...func_get_args());
+    return new HTML('em', func_get_args());
 }
 function embed(): HTML
 {
-    return HTML::embed(...func_get_args());
+    return new HTML('embed', func_get_args());
 }
 function fieldset(): HTML
 {
-    return HTML::fieldset(...func_get_args());
+    return new HTML('fieldset', func_get_args());
 }
 function figcaption(): HTML
 {
-    return HTML::figcaption(...func_get_args());
+    return new HTML('figcaption', func_get_args());
 }
 function figure(): HTML
 {
-    return HTML::figure(...func_get_args());
+    return new HTML('figure', func_get_args());
 }
 function footer(): HTML
 {
-    return HTML::footer(...func_get_args());
+    return new HTML('footer', func_get_args());
 }
 function form(): HTML
 {
-    return HTML::form(...func_get_args());
+    return new HTML('form', func_get_args());
 }
 function h1(): HTML
 {
-    return HTML::h1(...func_get_args());
+    return new HTML('h1', func_get_args());
 }
 function h2(): HTML
 {
-    return HTML::h2(...func_get_args());
+    return new HTML('h2', func_get_args());
 }
 function h3(): HTML
 {
-    return HTML::h3(...func_get_args());
+    return new HTML('h3', func_get_args());
 }
 function h4(): HTML
 {
-    return HTML::h4(...func_get_args());
+    return new HTML('h4', func_get_args());
 }
 function h5(): HTML
 {
-    return HTML::h5(...func_get_args());
+    return new HTML('h5', func_get_args());
 }
 function h6(): HTML
 {
-    return HTML::h6(...func_get_args());
+    return new HTML('h6', func_get_args());
 }
 function head(): HTML
 {
-    return HTML::head(...func_get_args());
+    return new HTML('head', func_get_args());
 }
 function header(): HTML
 {
-    return HTML::header(...func_get_args());
+    return new HTML('header', func_get_args());
 }
 function hgroup(): HTML
 {
-    return HTML::hgroup(...func_get_args());
+    return new HTML('hgroup', func_get_args());
 }
 function hr(): HTML
 {
-    return HTML::hr(...func_get_args());
+    return new HTML('hr', func_get_args());
 }
 function html(): HTML
 {
-    return HTML::html(...func_get_args());
+    return new HTML('html', func_get_args());
 }
 function i(): HTML
 {
-    return HTML::i(...func_get_args());
+    return new HTML('i', func_get_args());
 }
 function iframe(): HTML
 {
-    return HTML::iframe(...func_get_args());
+    return new HTML('iframe', func_get_args());
 }
 function img(): HTML
 {
-    return HTML::img(...func_get_args());
+    return new HTML('img', func_get_args());
 }
 function input(): HTML
 {
-    return HTML::input(...func_get_args());
+    return new HTML('input', func_get_args());
 }
 function ins(): HTML
 {
-    return HTML::ins(...func_get_args());
+    return new HTML('ins', func_get_args());
 }
 function kbd(): HTML
 {
-    return HTML::kbd(...func_get_args());
+    return new HTML('kbd', func_get_args());
 }
 function label(): HTML
 {
-    return HTML::label(...func_get_args());
+    return new HTML('label', func_get_args());
 }
 function legend(): HTML
 {
-    return HTML::legend(...func_get_args());
+    return new HTML('legend', func_get_args());
 }
 function li(): HTML
 {
-    return HTML::li(...func_get_args());
+    return new HTML('li', func_get_args());
 }
 function link(): HTML
 {
-    return HTML::link(...func_get_args());
+    return new HTML('link', func_get_args());
 }
 function main(): HTML
 {
-    return HTML::main(...func_get_args());
+    return new HTML('main', func_get_args());
 }
 function map(): HTML
 {
-    return HTML::map(...func_get_args());
+    return new HTML('map', func_get_args());
 }
 function mark(): HTML
 {
-    return HTML::mark(...func_get_args());
+    return new HTML('mark', func_get_args());
 }
 function menu(): HTML
 {
-    return HTML::menu(...func_get_args());
+    return new HTML('menu', func_get_args());
 }
 function meta(): HTML
 {
-    return HTML::meta(...func_get_args());
+    return new HTML('meta', func_get_args());
 }
 function meter(): HTML
 {
-    return HTML::meter(...func_get_args());
+    return new HTML('meter', func_get_args());
 }
 function nav(): HTML
 {
-    return HTML::nav(...func_get_args());
+    return new HTML('nav', func_get_args());
 }
 function noscript(): HTML
 {
-    return HTML::noscript(...func_get_args());
+    return new HTML('noscript', func_get_args());
 }
 function object(): HTML
 {
-    return HTML::object(...func_get_args());
+    return new HTML('object', func_get_args());
 }
 function ol(): HTML
 {
-    return HTML::ol(...func_get_args());
+    return new HTML('ol', func_get_args());
 }
 function optgroup(): HTML
 {
-    return HTML::optgroup(...func_get_args());
+    return new HTML('optgroup', func_get_args());
 }
 function option(): HTML
 {
-    return HTML::option(...func_get_args());
+    return new HTML('option', func_get_args());
 }
 function output(): HTML
 {
-    return HTML::output(...func_get_args());
+    return new HTML('output', func_get_args());
 }
 function p(): HTML
 {
-    return HTML::p(...func_get_args());
+    return new HTML('p', func_get_args());
 }
 function picture(): HTML
 {
-    return HTML::picture(...func_get_args());
+    return new HTML('picture', func_get_args());
 }
 function pre(): HTML
 {
-    return HTML::pre(...func_get_args());
+    return new HTML('pre', func_get_args());
 }
 function progress(): HTML
 {
-    return HTML::progress(...func_get_args());
+    return new HTML('progress', func_get_args());
 }
 function q(): HTML
 {
-    return HTML::q(...func_get_args());
+    return new HTML('q', func_get_args());
 }
 function rp(): HTML
 {
-    return HTML::rp(...func_get_args());
+    return new HTML('rp', func_get_args());
 }
 function rt(): HTML
 {
-    return HTML::rt(...func_get_args());
+    return new HTML('rt', func_get_args());
 }
 function ruby(): HTML
 {
-    return HTML::ruby(...func_get_args());
+    return new HTML('ruby', func_get_args());
 }
 function s(): HTML
 {
-    return HTML::s(...func_get_args());
+    return new HTML('s', func_get_args());
 }
 function samp(): HTML
 {
-    return HTML::samp(...func_get_args());
+    return new HTML('samp', func_get_args());
 }
 function script(): HTML
 {
-    return HTML::script(...func_get_args());
+    return new HTML('script', func_get_args());
 }
 function section(): HTML
 {
-    return HTML::section(...func_get_args());
+    return new HTML('section', func_get_args());
 }
 function select(): HTML
 {
-    return HTML::select(...func_get_args());
+    return new HTML('select', func_get_args());
 }
 function slot(): HTML
 {
-    return HTML::slot(...func_get_args());
+    return new HTML('slot', func_get_args());
 }
 function small(): HTML
 {
-    return HTML::small(...func_get_args());
+    return new HTML('small', func_get_args());
 }
 function source(): HTML
 {
-    return HTML::source(...func_get_args());
+    return new HTML('source', func_get_args());
 }
 function span(): HTML
 {
-    return HTML::span(...func_get_args());
+    return new HTML('span', func_get_args());
 }
 function strong(): HTML
 {
-    return HTML::strong(...func_get_args());
+    return new HTML('strong', func_get_args());
 }
 function style(): HTML
 {
-    return HTML::style(...func_get_args());
+    return new HTML('style', func_get_args());
 }
 function sub(): HTML
 {
-    return HTML::sub(...func_get_args());
+    return new HTML('sub', func_get_args());
 }
 function summary(): HTML
 {
-    return HTML::summary(...func_get_args());
+    return new HTML('summary', func_get_args());
 }
 function sup(): HTML
 {
-    return HTML::sup(...func_get_args());
+    return new HTML('sup', func_get_args());
 }
 function table(): HTML
 {
-    return HTML::table(...func_get_args());
+    return new HTML('table', func_get_args());
 }
 function tbody(): HTML
 {
-    return HTML::tbody(...func_get_args());
+    return new HTML('tbody', func_get_args());
 }
 function td(): HTML
 {
-    return HTML::td(...func_get_args());
+    return new HTML('td', func_get_args());
 }
 function template(): HTML
 {
-    return HTML::template(...func_get_args());
+    return new HTML('template', func_get_args());
 }
 function textarea(): HTML
 {
-    return HTML::textarea(...func_get_args());
+    return new HTML('textarea', func_get_args());
 }
 function tfoot(): HTML
 {
-    return HTML::tfoot(...func_get_args());
+    return new HTML('tfoot', func_get_args());
 }
 function th(): HTML
 {
-    return HTML::th(...func_get_args());
+    return new HTML('th', func_get_args());
 }
 function thead(): HTML
 {
-    return HTML::thead(...func_get_args());
+    return new HTML('thead', func_get_args());
 }
 function time(): HTML
 {
-    return HTML::time(...func_get_args());
+    return new HTML('time', func_get_args());
 }
 function title(): HTML
 {
-    return HTML::title(...func_get_args());
+    return new HTML('title', func_get_args());
 }
 function tr(): HTML
 {
-    return HTML::tr(...func_get_args());
+    return new HTML('tr', func_get_args());
 }
 function track(): HTML
 {
-    return HTML::track(...func_get_args());
+    return new HTML('track', func_get_args());
 }
 function u(): HTML
 {
-    return HTML::u(...func_get_args());
+    return new HTML('u', func_get_args());
 }
 function ul(): HTML
 {
-    return HTML::ul(...func_get_args());
+    return new HTML('ul', func_get_args());
 }
 function video(): HTML
 {
-    return HTML::video(...func_get_args());
+    return new HTML('video', func_get_args());
 }
 function wbr(): HTML
 {
-    return HTML::wbr(...func_get_args());
+    return new HTML('wbr', func_get_args());
 }
