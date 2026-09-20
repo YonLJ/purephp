65fc2387e862ae575e5a00c6bfad5a50e41f6a3b
YonLD <istintin@qq.com>
2026-09-20 20:12:00 +0800

    * bump dev dependencies and move the Utils functions into functions.php

--- src/Utils/functions.php
+++ src/Utils/functions.php
@@ -50,3 +50,30 @@ function clx(array|bool|int|float|string|null ...$args): string|null
 
     return $classList === [] ? null : implode(' ', $classList);
 }
+
+/**
+ * Join style declarations into an inline style attribute value.
+ *
+ * @param array<array-key, mixed>|null $list
+ * @return string|null The joined style string, or null if empty.
+ */
+function sty(array|null $list): string|null
+{
+    if (empty($list)) {
+        return null;
+    }
+
+    /** @var string[] */
+    $styleList = [];
+    foreach ($list as $key => $val) {
+        if (is_string($key) && (is_string($val) || is_numeric($val))) {
+            $styleList[] = "$key: $val";
+        }
+    }
+
+    if (empty($styleList)) {
+        return null;
+    }
+
+    return join('; ', $styleList) . ';';
+}
